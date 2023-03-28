54e7cf773801a086cfdf05f5f134762127d8431c
n.felix <miaounix@gmail.com>
2023-03-28 10:04:46 +0200

    fixing typo

--- dossier-docs/src/main/resources/docs/cmis/files/cmis_dossier_getcontentstream.java
+++ dossier-docs/src/main/resources/docs/cmis/files/cmis_dossier_getcontentstream.java
@@ -14,24 +14,24 @@ public ContentStream getContentStream(Session session, String objectId)
 public String downloadContentStream(Session session, String objectId)
     throws Exception{
 
-    try{
-      // renvoie le chemin absolu vers le fichier téléchargé
-      return cmisClient.downloadContentStream(session, objectId);
-    }
-    catch(...){
-    }
-    }
+  try{
+    // renvoie le chemin absolu vers le fichier téléchargé
+    return cmisClient.downloadContentStream(session, objectId);
+  }
+  catch(...){
+  }
+}
 # end::getall[]
 
 # tag::getpartial[]
 public ContentStream getPartialContentStream(Session session, String objectId, long offset, long length)
     throws Exception{
 
-    try{
+  try{
     // attention, ContentStream#getStream, si utilisé, doit être fermé !
     return cmisClient.getContentStream(session, objectId, offset,  length );
-    }
-    catch(...){
-    }
-    }
+  }
+  catch(...){
+  }
+}
 # end::getpartial[]
